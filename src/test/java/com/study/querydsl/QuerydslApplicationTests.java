7aa2b4149c436e2eab56c4a3af219fc9d65128c0
sonhyangji <ths3437@naver.com>
2021-07-04 18:56:49 +0900

    2021-07-04

--- src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,39 @@
 package com.study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.entity.Hello;
+import com.study.querydsl.entity.QHello;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
+@Commit
 class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        Assertions.assertThat(result).isEqualTo(hello);
+        //lombok 동작 확인 (hello.getId())
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
